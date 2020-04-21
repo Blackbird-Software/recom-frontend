311dd797cff90f75e5a810fa9ece0ea029bd9e72
Grzegorz Wawrzak <grzegorz.wawrzak@softnauts.com>
2020-04-22 03:19:18 +0900

    Init

---
+++ components/BookList.vue
@@ -0,0 +1,53 @@
+<template>
+
+  <div>
+    <b-card-group columns>
+
+      <b-card
+        :title="book.title"
+        img-src="https://placekitten.com/g/400/450"
+        img-alt="Image"
+        img-top
+        v-for="book in books"
+      >
+        <b-card-text>
+          {{book.author}}
+        </b-card-text>
+      </b-card>
+
+    </b-card-group>
+  </div>
+
+</template>
+
+<script lang="ts">
+  import Vue from 'vue'
+
+  export default Vue.extend({
+    name: 'BookList',
+    data() {
+      return {
+        books: [
+          {
+            id: 1,
+            title: 'Winnie The Pooh',
+            author: 'A. A. Milne',
+            publishedAt: 2018
+          },
+          {
+            id: 2,
+            title: 'Norwegian Wood',
+            author: 'H. Murakami',
+            publishedAt: 2020
+          },
+          {
+            id: 3,
+            title: 'Dziady',
+            author: 'A. Mickiewicz',
+            publishedAt: 2020
+          }
+        ]
+      }
+    }
+  })
+</script>
---
+++ pages/index.vue
@@ -0,0 +1,22 @@
+<template>
+  <b-container fluid>
+
+    <navbar></navbar>
+
+    <book-list></book-list>
+
+  </b-container>
+</template>
+
+<script lang="ts">
+  import Vue from 'vue'
+  import Navbar from '~/components/Navbar.vue'
+  import BookList from '~/components/BookList.vue'
+
+  export default Vue.extend({
+    components: {
+      Navbar,
+      BookList
+    }
+  })
+</script>
